8cae9adb3be55e9689e318a6ff14ea79f94a07e7
0Sebbe <sebastian.stavenek@gmail.com>
2022-08-31 14:21:32 +0200

    Added write to file function
    reads from text files

--- Read and write/src/main.cpp
+++ Read and write/src/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 namespace nasp 
 {
-
 	template <typename T>
 	void Print(T t) 
 	{
 		std::cout << t << "\n";
 	}
+
+	void WriteToFile(std::string filePath) 
+	{
+		std::ofstream file(filePath);
+		std::string fileContent;
+		std::getline(std::cin, fileContent);
+		file << fileContent;
+		file.close();
+	}
+
+	std::string filePath = "myFile.txt";
+
 }
 
 int main()
 {
-	nasp::Print("ses");
+	nasp::Print("Skriv något:");
+	nasp::WriteToFile(nasp::filePath);
 }
